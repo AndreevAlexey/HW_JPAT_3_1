d52ece1f0b3c2b967824597f5c063124b1b187af
Andreev Alexey <andreev-j@yandex.ru>
2021-09-29 14:37:23 +0700

    Initial commit

---
+++ src/main/java/Randoms.java
@@ -0,0 +1,32 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    protected int min, max;
+
+    public Randoms(int min, int max) {
+        random = new Random();
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                if (max < min || min == 0 && max == 0 ) return false;
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                if ( !hasNext() ) {
+                    throw new java.util.NoSuchElementException();
+                }
+                return random.nextInt(max-min+1)+min;
+            }
+        };
+    }
+}
